d457e8c96c6225bcbccadd365b9c377abcff4075
Daniel Belenguer <danielbelenguerfernandez@gmail.com>
2025-02-19 17:19:17 +0100

    Se arregla el swith para que el tema pueda cambiarse de manera correcta y se guarde en el local storage

--- static/restaurante/app.js
+++ static/restaurante/app.js
@@ -2,26 +2,21 @@ document.addEventListener('DOMContentLoaded', function() {
     let $btn_tema = document.getElementById('tema');
     let $html_tema = document.querySelector('html');
 
-    // Tema guardado en localStorage al cargar la paguina
+    // Tema guardado en localStorage al cargar la página
     const temaGuardado = localStorage.getItem('data-theme');
     if (temaGuardado) {
         $html_tema.setAttribute('data-theme', temaGuardado);
-    }
-    // Inicializar el tema basado en el estado del checkbox
-    if ($btn_tema.checked) {
-        $html.setAttribute("data-theme", "light");
-    } else {
-        $html.setAttribute("data-theme", "dark");
+        $btn_tema.checked = temaGuardado === 'light';
     }
 
     // Escuchar cambios en el checkbox
-    $tema.addEventListener('change', function() {
+    $btn_tema.addEventListener('change', function() {
         if ($btn_tema.checked) {
             $html_tema.setAttribute("data-theme", "light");
-            localStorage.setItem('data_theme', 'light');
+            localStorage.setItem('data-theme', 'light');
         } else {
             $html_tema.setAttribute("data-theme", "dark");
-            localStorage.setItem('data_theme', 'dark');
+            localStorage.setItem('data-theme', 'dark');
         }
     });
 });
