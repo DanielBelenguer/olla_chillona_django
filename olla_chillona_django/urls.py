a1e392f2ea8866490a9782ba261503c69d9184c2
Daniel Belenguer <danielbelenguerfernandez@gmail.com>
2025-02-24 17:44:11 +0100

    Se arregla la template de login

--- olla_chillona_django/urls.py
+++ olla_chillona_django/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
+
 urlpatterns = [
     path("restaurante/", include("restaurante.urls")),
     path("admin/", admin.site.urls),
